328efeff5aedea890c6e4d34d362728e2584af98
probeshnath <probeshn@gmail.com>
2023-11-29 18:34:56 +0600

    stripe added and money cut from user

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 
@@ -225,6 +226,26 @@ async function run() {
       const result = await galleryDB.find().toArray();
       res.send(result)
     })
+
+
+    // payment intent
+    app.post("/create-payment-intent",async(req,res) =>{
+      const {price} = req.body;
+      const amount = parseInt(price * 100)
+      console.log("amount",amount)
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+
 
 
 
