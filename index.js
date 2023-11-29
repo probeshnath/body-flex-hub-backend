8ebfd90c448165651a6e0c846b8a92e1e5a83416
probeshnath <probeshn@gmail.com>
2023-11-29 19:53:08 +0600

    single cart aetch

--- index.js
+++ index.js
@@ -237,7 +237,13 @@ async function run() {
       const result = await cartClasseDB.insertOne(data);
       res.send(result)
     })
-
+    // get the singlr class
+    app.get("/cartClass/:id", async(req,res) =>{
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)}
+      const result = await cartClasseDB.findOne(query);
+      res.send(result);
+    })
 
 
 
